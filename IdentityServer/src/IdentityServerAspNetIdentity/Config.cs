d39d08cf0a3de87c92be43c535720827152fa621
Gian-z <gianluca.zwahlen@cmiag.ch>
2024-10-01 10:25:31 +0200

    import

---
+++ IdentityServer/src/IdentityServerAspNetIdentity/Config.cs
@@ -0,0 +1,75 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+using IdentityModel;
+
+namespace IdentityServerAspNetIdentity;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+            new IdentityResource
+            {
+                Name = "verification",
+                UserClaims = new List<string> 
+                { 
+                    JwtClaimTypes.Email,
+                    JwtClaimTypes.EmailVerified
+                }
+            }
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new[]
+        { 
+            new ApiScope(name: "api1", displayName: "My API")
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new[] 
+        {
+            new Client
+            {
+                ClientId = "client",
+
+                // no interactive user, use the clientid/secret for authentication
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                // secret for authentication
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+
+                // scopes that client has access to
+                AllowedScopes = { "api1" }
+            },
+            // interactive ASP.NET Core Web App
+            new Client
+            {
+                ClientId = "web",
+                ClientSecrets = { new Secret("secret".Sha256()) },
+
+                AllowedGrantTypes = GrantTypes.Code,
+                
+                // where to redirect to after login
+                RedirectUris = { "https://localhost:5002/signin-oidc" },
+
+                // where to redirect to after logout
+                PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+
+                AllowOfflineAccess = true,
+
+                AllowedScopes =
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    "verification",
+                    "api1"
+                }
+            }
+        };
+}
